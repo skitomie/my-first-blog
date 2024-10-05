4be71cfc53084cc9c0607fa7afb02eeed2f5a970
skitomie <skitomie@gmail.com>
2024-10-05 23:12:38 +0100

    update

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,7 @@ from django.utils import timezone
 
 def post_list(request):
     #posts= Post.objects.all()
+    print("view was called")
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'post_list.html', {'posts': posts})
+    #posts = Post.objects.all().order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
